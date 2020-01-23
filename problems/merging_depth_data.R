a23fc270777eecec90bb56124b662b94c2fa59ef
Shira Salingre <shira.salin@gmail.com>
2020-01-23 20:57:17 +0200

    problem with depth data

--- problems/merging_depth_data.R
+++ problems/merging_depth_data.R
@@ -6,13 +6,15 @@ new_data <- read_csv("data/all_uvc_data_201119.csv", na = c("", "NA"), col_types
 str(new_data)
 
 # MEData (data I want to update with depths)
-old_data <- read_csv("data/all_uvc_data_101119.csv", na = c("", "NA"), col_types = cols(depth = "d")) %>%
-  mutate(id = rownames(.), depth.x = depth) %>% select(-depth)
+old_data <- read_csv("data/medata_nov_2019.csv", na = c("", "NA"), col_types = cols(depth = "d")) %>%
+  mutate(depth.x = depth) %>% select(-depth) %>% mutate(id = rownames(.))
 str(old_data)
-
 summary(old_data)
+
 # Join the 2 together to get the depth
-new_old_data <- old_data %>% left_join(new_data)
+new_old_data <- old_data %>% left_join(depth_data)
+
+new_old_data %>% count(id) %>% arrange(desc(n))
 
 colnames(new_old_data)
 new_old_data <- new_old_data %>%
@@ -23,22 +25,14 @@ new_old_data <- new_old_data %>%
 
 summary(new_old_data)
 
-depth_miss <- new_old_data %>% filter(is.na(depth)) %>% distinct(site) %>% print(n = Inf)
-new_depth_miss <- new_data %>% filter(is.na(depth)) %>% distinct(site) %>% print(n = Inf)
-old_depth_miss <- old_data %>% filter(is.na(depth.x)) %>% distinct(site) %>% print(n = Inf)
+old_data %>% 
+  sample_n(1000) %>% 
+  left_join(depth_data) %>% 
+  count(id) %>% 
+  arrange(desc(n))
 
-setdiff(old_depth_miss, new_depth_miss) %>% print(n = Inf)
-
-
-new_old_data <- new_old_data %>% mutate(id = row.names(.))
-
-crete_data <- read_csv("data/crete_uvc_full.csv")
-# crete_depth <- crete_data %>% select(site, lon, lat, trans, depth) %>% distinct()
-
-full_data <- old_data %>% left_join(crete_data)
-summary(full_data)
-
-check <- full_data %>% count(id) %>% arrange(desc(n)) %>% distinct() %>% filter(n > 1) %>% .$id
-check_full <- full_data[check,]
-View(check_full)
+new_data %>% 
+  filter(site == "ASINARA_add", trans == "48") %>% 
+  select(lon, lat, site, trans, depth) %>% 
+  distinct()
 
