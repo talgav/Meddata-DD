54f94971701e301c69d0ded3ff0fb60405e5bdf9
Shira Salingre <shira.salin@gmail.com>
2020-01-23 20:03:54 +0200

    crete data corrected some NAs in salinity, a and b

--- R/add_crete_data_to UVC.R
+++ R/add_crete_data_to UVC.R
@@ -7,21 +7,41 @@ count(medata)
 uvc_data <- read_csv("data/all_uvc_data_101119.csv", na = c("", "NA"), col_types = cols(depth = "d")) %>%
   filter(!str_detect(.$site, "Cret"))
 
-count(uvc_data)
+count(medata) - count(uvc_data)
 
-a <- uvc_data %>% select(site, lon, lat, trans, depth) %>% 
+a <- uvc_data %>% dplyr::select(site, lon, lat, trans, depth) %>% 
   distinct()
 
 a %>% count(site,lon,lat,trans) %>% arrange(desc(n))
 
 crete <- read_csv("data/crete_uvc.csv")
 
+# check there's no depth duplicates
+a <- crete %>% select(site, lon, lat, trans, depth) %>% 
+  distinct()
+a %>% count(site,lon,lat,trans) %>% arrange(desc(n))
+a %>% filter(site == "ASSECret2210191MLSC") %>% count(site, lon, lat, trans)
+a %>% filter(site == "ASSECret2210191MLSC") %>% filter(trans == 1)
+# shit
+
+crete_raw <- read_csv("~/MSc/Lab/crete2019/UVC_crete_2019.csv", col_types = cols(Notes = "c"))
+colnames(crete_raw)
+crete_raw %>%
+  filter(SiteID == "ASSECret2210191MLSC") %>%
+  distinct(SiteID, Transect, Mean_Depth)
+
+
+
 # check there's no differences in columns before I merge them
-cols_crete <- colnames(crete)
+cols_crete <- colnames(crete_full)
 cols_uvc <- colnames(uvc_data)
 
 setdiff(cols_crete, cols_uvc)
 
-uvc_full <- bind_rows(uvc_data, crete)
+uvc_full <- bind_rows(uvc_data, crete_full)
 uvc_full %>% distinct(country)
 count(uvc_full)
+
+summary(uvc_full)
+
+# write_csv(uvc_full, "data/medata_nov_2019.csv")
--- R/modifying_uvc_data.R
+++ R/modifying_uvc_data.R
@@ -19,13 +19,34 @@ crete_data$Species <- str_replace(crete_data$Species, "Gobiidae spp", "Gobiidae"
 crete_data$Species <- str_replace(crete_data$Species, "Belonidae spp.", "Belonidae")
 crete_data$Species <- str_replace(crete_data$Species, "Pagrus coeruleostrictus", "Pagrus coeruleostictus")
 
-colnames(crete_data)
+str(crete_data)
 
 crete_data <- crete_data %>%
   filter(Distance <= 2.5) %>% # limit belt transect to 2.5 m distance
   filter(Confidence == 1) %>% # only species that we're sure about their ID
   filter(Date != 2019-10-12 & Observer_Name != "Shira Salingre",
          Date != 2019-10-13 & Observer_Name != "Shira Salingre") # remove inexperienced observer from 2 1st days of diving
+# ---------
+# Check there are no duplicates with depths (2 depths for the same transect)
+crete_data %>%
+  select(lon, lat, SiteID, Transect, Mean_Depth) %>%
+  distinct() %>%
+  count(lon, lat, SiteID, Transect) %>%
+  arrange(desc(n))
+
+# There are 2 depths for transect ASSECret2210191MLSC_A:
+# when I check it in the raw data (google sheet) I find that there are 2 transects with the same name
+crete_data %>%
+  select(lon, lat, SiteID, Transect, Mean_Depth) %>%
+  filter(SiteID == "ASSECret2210191MLSC") %>%
+  distinct()
+
+# change the transect ID
+crete_data[crete_data$SiteID == "ASSECret2210191MLSC" & crete_data$`Time Start` > hms::hms(hours = 16),]$SiteID <- "ASSECret2210192MLSC"
+
+
+
+# ---------
 
 # choose one observer at random
 crete <- data.frame()
@@ -46,7 +67,7 @@ crete$trans <- ifelse(crete$Transect == "A", 1,
 crete_mod <- crete %>% mutate(data.origin = "Belmaker", country = "Greece", # add columns of UVC format
                               protection = "NO", enforcement = 0, total.mpa.ha	= 0,
                               size.notake = 0, yr.creation	= NA, age.reserve.yr = NA,
-                              site = SiteID) %>%
+                              site = TransID) %>%
                        select(data.origin, country, site, lon, lat, trans, Species, Length, Amount,
                              Season, protection, enforcement, total.mpa.ha, size.notake, yr.creation,
                              age.reserve.yr, Mean_Depth)
@@ -54,13 +75,20 @@ crete_mod <- crete %>% mutate(data.origin = "Belmaker", country = "Greece", # ad
 colnames(crete_mod) <- c(uvc_colnames)
 crete_mod$species <- gsub("\\s", "\\.", x = crete_mod$species) # replace space with '.' in species names
 
+# Check there are no duplicates with depths (2 depths for the same transect)
+crete_mod %>%
+  select(site, lon, lat, trans, depth) %>%
+  distinct() %>%
+  count(site,lon,lat,trans) %>%
+  arrange(desc(n))
+
 # check it out
 glimpse(crete_mod)
 
 # write_csv(crete_mod, "data/crete_uvc.csv")
 
 # add to medata:
-medata <- read_csv("data/all_uvc_.csv")
+medata <- read_csv("data/uvc_data_200120.csv")
 colnames(medata)
 
 # check for new species:
@@ -74,7 +102,7 @@ crete_full <- crete_mod %>% dplyr::left_join(ab)
 
 # add environmental variables (explanations in `bio-oracle extraction code.R`)
 library(sdmpredictors)
-unique_coords <- crete_full %>% 
+unique_coords <- crete_mod %>% 
   dplyr::distinct(lon, lat)
 temperature <- load_layers(c("BO_sstmean", "BO_sstrange"), datadir = "~/MEData/Bio ORACLE data")
 salinity <- load_layers("BO2_salinitymean_bdmin", datadir = "~/MEData/Bio ORACLE data")
@@ -92,11 +120,11 @@ pp_range <- pp_range[, "BO2_pprange_bdmin"]
 env_data <- cbind.data.frame(unique_coords, tmean = temp_mean, trange = temp_range, sal_mean, pp_mean, pp_range) # put together all enviromental data with coordiantes
 
 # add data to crete_full file:
-crete_full <- crete_full %>%
+crete_full <- crete_mod %>%
   left_join(env_data, by = c("lon", "lat"))
 
 View(crete_full)
-# write_csv(crete_full, "crete_uvc_full.csv")
+# write_csv(crete_full, "data/crete_uvc_full.csv")
 
 # FIXME salinity not filled for some reason?
 
