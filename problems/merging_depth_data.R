5d836d4d94683ca97c0970a61d2363d1a7fa49f4
Shira Salingré <salingre@mail.tau.ac.il>
2020-02-19 10:50:06 +0200

    some more checks on the data

--- problems/merging_depth_data.R
+++ problems/merging_depth_data.R
@@ -1,29 +1,33 @@
 library(tidyverse)
 
-medata <- read_csv("data/uvc_data_200120.csv")
+medata <- read_csv("data/uvc_data_200120.csv", col_types = cols(total.mpa.ha = col_double(), size.notake = col_double()))
 summary(medata)
 medata %>% filter(site != "Cret12", site != "Cret13", site != "Cret14", 
                   site != "Cret16", site != "Cret18", site != "Cret19",
                   site != "Cret20", site != "Cret22") %>% count(is.na(depth))
 
-new_data %>% count(is.na(depth))
+# medata %>% filter(country == "Greece") %>% select(site) %>% distinct()
 
-medata %>% filter(country == "Greece") %>% select(site) %>% distinct()
-
-# Data from Ori (from which I'll extract depth)
+## Data from Ori (from which I'll extract depth)
 new_data <- read_csv("data/all_uvc_data_201119.csv", na = c("", "NA"), col_types = cols(country = "c"))
-(depth_data <- new_data %>% select(site, lon, lat, trans, depth) %>% distinct())
-str(new_data)
+depth_data <- new_data %>% select(site, lon, lat, trans, depth) %>% distinct()
+summary(new_data)
 
-# MEData (data I want to update with depths)
+# new_data %>% count(is.na(depth))
+
+## MEData (data I want to update with depths)
 old_data <- read_csv("data/medata_nov_2019.csv", na = c("", "NA"), col_types = cols(depth = "d")) %>%
   mutate(depth.x = depth) %>% select(-depth) %>% mutate(id = rownames(.))
 str(old_data)
 summary(old_data)
 
-# Join the 2 together to get the depth
+## No duplicated depth values for `new_data` or`old_data` or `depth_data` by themselves (only when merged)
+
+
+## Join the 2 together to get the depth
 new_old_data <- old_data %>% left_join(depth_data)
 
+## Check for duplicate depths (same point with > 1 depth value)
 new_old_data %>% count(id) %>% arrange(desc(n))
 
 colnames(new_old_data)
