b52949c5b0de57af9969e304da1e278a2ba69360
Shira Salingre <shira.salin@gmail.com>
2021-05-17 13:18:47 +0300

    medata join with lessepsian migrants information created duplicates which were removed, but now medata does not include exotic species information

--- R/wrangle_medata.R
+++ R/wrangle_medata.R
@@ -40,7 +40,7 @@ table(med_raw$protection)
 
 # Sites that are outside of MPAs (protection == FALSE) should have `total.mpa.ha` and `size.notake` set to NA
 med_raw <- med_raw %>% mutate(total.mpa.ha = ifelse(protection == FALSE & total.mpa.ha == 0, NA, total.mpa.ha),
-                    size.notake = ifelse(protection == FALSE & size.notake == 0, NA, size.notake))
+                              size.notake = ifelse(protection == FALSE & size.notake == 0, NA, size.notake))
 
 
 # Depth -------------------------------------------------------------------
@@ -53,6 +53,7 @@ med_raw %>%
   count(site,lon,lat,trans) %>%
   arrange(desc(n))
 
+# ...Check them out...
 med_raw %>%
   dplyr::select(site, lon, lat, trans, depth) %>%
   distinct() %>%
@@ -66,14 +67,11 @@ med_raw[which(med_raw$site == "assecret2210191mlsc_c"),]$depth %<>% mean()
 
 # Redundancy --------------------------------------------------------------
 
-# Minimise unnecessary rows so that every observation of a species, of a size, in a location (site-trans) will recieve one row:
+# Minimise unnecessary rows so that multiple counts of the same species in a transect
+# will recieve one row (observation) by summing up count of individual:
 medata <- med_raw %>% 
-  group_by(data.origin, country, site, trans, lon, lat, season, 
-           protection, enforcement, total.mpa.ha, size.notake,
-           yr.creation, age.reserve.yr, depth, tmean, 
-           trange, sal_mean, pp_mean, pp_range,
-           species, sp.length, a, b) %>% 
-  summarise(abundance = sum(sp.n)) %>% 
+  group_by_at(setdiff(names(.), "sp.n")) %>% 
+  summarise(abundance = sum(sp.n), .groups = "drop") %>% 
   rename(sp.n = abundance)
 
 # check that there is no data loss:
@@ -86,6 +84,8 @@ sum(med_raw$sp.n) == sum(medata$sp.n)
 indie_raw <- read_csv("data/ab_data_med_hezibuba.csv") %>% 
   filter(!is.na(species)) %>% 
   select(species, les)
+
+# Add missing
 indie_data <- indie_raw %>% 
   mutate(les = case_when(species == "Abudefduf saxatilis" ~ 1,
                          species == "Balistes carolinensis" ~ 0,
@@ -110,59 +110,69 @@ indie_data <- indie_raw %>%
             exotic = as.logical(les))
 
 # checking that the conversion went ok
-table(indie_raw$les)
-table(indie_data$exotic)
+indie_raw %>% 
+  count(les)
+indie_data %>% 
+  count(exotic)
 
-medata %<>% 
-  select(-exotic) %>% 
+# Check species names match medata species names before joining:
+species_in_medata_only <- setdiff(medata %>% distinct(species) %>% arrange() %>% pull(), 
+        indie_data %>% distinct(species) %>% arrange() %>% pull())
+
+# TODO: Add missing species (replace lines 135-175 with the following code (to be written):
+
+medata <- medata %>% 
   left_join(indie_data, by = "species")
 
 # Check for any species that are still 'exotic' == NA:
 medata %>% 
   ungroup() %>%
   filter(is.na(exotic)) %>%
-  distinct(species, exotic)
+  distinct(species, exotic) %>% 
+  arrange(species) %>% 
+  print(n = Inf)
 
-medata %<>% mutate(exotic = case_when(species == "Mugil.cephalus" |
-                                        species == "Labrus.merula" |
-                                        species == "Labrus.viridis" |
-                                        species == "Symphodus.melanocercus" |
-                                        species == "Gobius.cruentatus" |
-                                        species == "Parablennius.rouxi" |
-                                        species == "Tripterygion.tripteronotus" |
-                                        species == "Symphodus.ocellatus" |
-                                        species == "Tripterygion.delaisi" |
-                                        species == "Symphodus.rostratus"|
-                                        species == "Labrus.mixtus" |
-                                        species == "Gobius.bucchichi" |
-                                        species == "Symphodus.cinereus" |
-                                        species == "Parablennius.zvonimiri" |
-                                        species == "Tripterygion.melanurus" |
-                                        species == "Parablennius.sanguinolentus" |
-                                        species == "Oedalechilus.labeo" |
-                                        species == "Gobius.paganellus" |
-                                        species == "Parablennius.pilicornis" |
-                                        species == "Belone.belone" |
-                                        species == "Scartella.cristata" |
-                                        species == "Lichia.amia" |
-                                        species == "Gobius.xanthocephalus" |
-                                        species == "Pomatoschistus.quagga" |
-                                        species == "Parablennius.tentacularis" |
-                                        species == "Gobius.auratus" |
-                                        species == "Gobius.cobitis" |
-                                        species == "Gobius.geniporus" |
-                                        species == "Mullus.barbatus" |
-                                        species == "Epinephelus.caninus" |
-                                        species == "Trisopterus.minutus" |
-                                        species == "Gobius.vittatus" |
-                                        species == "Chelon.labrosus"
-                                      ~ FALSE,
-                                      species == "Cheilodipterus.novemstriatus" |
-                                        species == "Pterois.miles" |
-                                        species == "Parupeneus.forsskali" |
-                                        species == "Herklotsichthys.punctatus"
-                                      ~ TRUE,
-                                      TRUE ~ exotic))
+medata <- medata %>% 
+  mutate(exotic = case_when(species == "Mugil.cephalus" |
+                              species == "Labrus.merula" |
+                              species == "Labrus.viridis" |
+                              species == "Symphodus.melanocercus" |
+                              species == "Gobius.cruentatus" |
+                              species == "Parablennius.rouxi" |
+                              species == "Tripterygion.tripteronotus" |
+                              species == "Symphodus.ocellatus" |
+                              species == "Tripterygion.delaisi" |
+                              species == "Symphodus.rostratus"|
+                              species == "Labrus.mixtus" |
+                              species == "Gobius.bucchichi" |
+                              species == "Symphodus.cinereus" |
+                              species == "Parablennius.zvonimiri" |
+                              species == "Tripterygion.melanurus" |
+                              species == "Parablennius.sanguinolentus" |
+                              species == "Oedalechilus.labeo" |
+                              species == "Gobius.paganellus" |
+                              species == "Parablennius.pilicornis" |
+                              species == "Belone.belone" |
+                              species == "Scartella.cristata" |
+                              species == "Lichia.amia" |
+                              species == "Gobius.xanthocephalus" |
+                              species == "Pomatoschistus.quagga" |
+                              species == "Parablennius.tentacularis" |
+                              species == "Gobius.auratus" |
+                              species == "Gobius.cobitis" |
+                              species == "Gobius.geniporus" |
+                              species == "Mullus.barbatus" |
+                              species == "Epinephelus.caninus" |
+                              species == "Trisopterus.minutus" |
+                              species == "Gobius.vittatus" |
+                              species == "Chelon.labrosus"
+                            ~ FALSE,
+                            species == "Cheilodipterus.novemstriatus" |
+                              species == "Pterois.miles" |
+                              species == "Parupeneus.forsskali" |
+                              species == "Herklotsichthys.punctatus"
+                            ~ TRUE,
+                            TRUE ~ exotic))
 
 # Add missing length-weight contnstants:
 library("rfishbase")
@@ -180,7 +190,7 @@ species_constants <- rfishbase::length_weight(missing_constants$species, fields 
   rename(species = Species) %>% 
   mutate(species = str_replace(.$species, pattern = "\\ ", "\\."))
 
-medata %<>% 
+medata <- medata %>% 
   ungroup() %>% 
   left_join(species_constants, by = "species") %>%
   mutate(a = if_else(is.na(a), mean_a, a),
@@ -192,8 +202,12 @@ medata %>%
   filter(is.na(a)) %>%
   distinct(species) %>% 
   print(n = Inf)
-  
+
 # # Check for fish_networks proj (which of left NAs are of my species?):
+# groupers <- c("Epinephelus.costae", "Epinephelus.marginatus", "Serranus.cabrilla", "Serranus.scriba")
+# diplodus <- c("Diplodus.annularis", "Diplodus.puntazzo", "Diplodus.sargus", "Diplodus.vulgaris")
+# herbivores <- c("Siganus.rivulatus", "Siganus.luridus", "Sarpa.salpa", "Sparisoma.cretense")
+# 
 # species_constants %>%
 #   filter(is.na(mean_a)) %>%
 #   filter(species %in% c(groupers, diplodus, herbivores))
@@ -202,34 +216,12 @@ medata %>%
 
 # Save the new file -------------------------------------------------------
 
-# create an Rdata file of the dataset after all changes:
+medata
+
 # write_rds(medata, "data/medata.Rds")
-# copy to my project (personal laptop)
-# file.copy("data/medata.Rds",
-#           "C:/Users/shira/Documents/MSc/fish_social_network/data/medata.Rds",
-#           overwrite = TRUE)
 # # copy to my project (personal laptop)
 # file.copy("data/medata.Rds",
-#           "C:/Users/Shira/Documents/R/fish_social_network/data/medata.Rds",
+#           "C:/Users/shira/Documents/MSc/Paper/MedFishCooccurrence/data/medata.Rds",
 #           overwrite = TRUE)
-
-# # load the data back after changes:
-# medata <- read_rds("data/medata.Rds")
-
-# Not fixed yet -----------------------------------------------------------
-
-# locations without salinity data:
-medata %>% filter(is.na(sal_mean)) %>% distinct(site)
-# Fix salinity
-library(raster)
-library(sf)
-med_sal <- medata %>% filter(is.na(sal_mean)) # filter all the NAs of salinity
-med_sf <- sf::st_as_sf(med_sal, coords = c("lon", "lat"), crs = 4326) # convert df to sf
-med_sf %<>% sf::st_as_sf() %>% sf::st_transform(crs = 3857) # transform to a WGS84 in meters for buffer creation
-med_buffer <- sf::st_buffer(x = med_sf, dist = 50000) %>% sf::st_transform(crs = 4326) # create a buffer and transform back to crs in lat-lon (ESPG:4326)
-# sdmpredictors::load_layers(layercodes = "BO2_salinitymean_ss", datadir = "data/bio_oracle") # import salinity raster from bio-oracle
-sal_rast <- raster("data/bio_oracle/BO2_salinitymean_ss_lonlat.tif") # load the salinity raster (crs 4326)
-salinity_vals <- raster::extract(x = sal_rast, y = med_buffer, fun = mean, df = TRUE) # extract salinity values for each polygon buffer
-
 
 
